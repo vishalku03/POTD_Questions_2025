23f31ef26c4a555bad6055488469652d21c75bb9
Vishal Kumar <vishalk000123@gmail.com>
2025-01-06 21:32:06 +0530

    lc

---
+++ day_6/1769. Minimum Number of Operations to Move All Balls to Each Boxv.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+     vector<int> minOperations(string boxes)
+     {
+
+          vector<int> ans;
+          int n = boxes.size();
+
+          for (int i = 0; i < n; i++)
+          {
+               int count = 0;
+
+               for (int j = 0; j < n; j++)
+               {
+                    if (boxes[j] == '1')
+                         count = count + abs(i - j);
+               }
+               ans.push_back(count);
+          }
+
+          return ans;
+     }
+};
