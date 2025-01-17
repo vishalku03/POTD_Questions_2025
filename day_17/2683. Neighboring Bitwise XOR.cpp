a9f61413d50d2c3707558baa97631e0b38a1bfa3
Vishal Kumar <vishalk000123@gmail.com>
2025-01-17 22:37:30 +0530

    lc

---
+++ day_17/2683. Neighboring Bitwise XOR.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+     bool doesValidArrayExist(vector<int> &derived)
+     {
+          int xorSum = 0;
+          for (int num : derived)
+          {
+               xorSum ^= num;
+          }
+          return xorSum == 0;
+     }
+};
