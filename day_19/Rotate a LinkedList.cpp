92ef6bf1021bf9def3b09d77498dacf262118ce9
Vishal Kumar <vishalk000123@gmail.com>
2025-01-19 19:34:31 +0530

    gfg

---
+++ day_19/Rotate a LinkedList.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+     Node *rotate(Node *head, int k)
+     {
+          // Your code here
+          if (k == 0)
+               return head;
+          int len = 1;
+          Node *temp = head;
+          while (temp->next)
+          {
+               temp = temp->next;
+               len++;
+          }
+          k = k % len;
+          if (k == 0)
+          {
+               return head;
+          }
+          temp->next = head;
+          Node *nwt = head;
+          for (int i = 1; i < k; i++)
+               nwt = nwt->next;
+          Node *nwh = nwt->next;
+          nwt->next = NULL;
+          return nwh;
+     }
+};
+// gfg
