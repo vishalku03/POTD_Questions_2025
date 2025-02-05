660bdf774a3b60311162f5c57482fb44b1147187
Vishal Kumar <vishalk000123@gmail.com>
2025-02-05 20:05:06 +0530

    gfg

---
+++ day_36/Mirror tree.cpp
@@ -0,0 +1,29 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+public:
+     // Function to convert a binary tree into its mirror tree.
+     void mirror(Node *node)
+     {
+          if (!node)
+               return;
+          swap(node->left, node->right);
+          mirror(node->left);
+          mirror(node->right);
+     }
+};
+
+// gfg
