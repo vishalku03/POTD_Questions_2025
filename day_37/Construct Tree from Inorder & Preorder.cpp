0b39486d200c973ef5923a9b61595863d63f6f80
Vishal Kumar <vishalk000123@gmail.com>
2025-02-06 21:25:39 +0530

    gfg

---
+++ day_37/Construct Tree from Inorder & Preorder.cpp
@@ -0,0 +1,41 @@
+
+/*
+Definition of the Node class
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+  int idx=0;
+  Node* Solve(vector<int>&inorder,vector<int>&preorder,int s,int e){
+      if(s>e)
+      return NULL;
+      Node* root=new Node(preorder[idx++]);
+      int k;
+      for(int l=s;l<=e;l++){
+          if(inorder[l]==root->data){
+              k=l;
+              break;
+          }
+      }
+      root->left=Solve(inorder,preorder,s,k-1);
+      root->right=Solve(inorder,preorder,k+1,e);
+      return root;
+  }
+    // Function to build the tree from given inorder and preorder traversals
+    Node *buildTree(vector<int> &inorder, vector<int> &preorder) {
+       return Solve(inorder,preorder,0,inorder.size()-1);
+    }
+};
+
+//gfg
