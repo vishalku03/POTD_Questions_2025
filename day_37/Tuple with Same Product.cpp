c8f1e607ea8e962231f1265755bb6eead14fcab3
Vishal Kumar <vishalk000123@gmail.com>
2025-02-06 19:15:13 +0530

    lc

---
+++ day_37/Tuple with Same Product.cpp
@@ -0,0 +1,23 @@
+
+class Solution
+{
+public:
+     int tupleSameProduct(std::vector<int> &nums)
+     {
+          std::unordered_map<int, int> map;
+          int count = 0;
+
+          for (int i = 0; i < nums.size(); i++)
+          {
+               for (int j = i + 1; j < nums.size(); j++)
+               {
+                    int product = nums[i] * nums[j];
+                    count += (map[product] * 8);
+                    map[product]++;
+               }
+          }
+          return count;
+     }
+};
+
+// lc - 1726
