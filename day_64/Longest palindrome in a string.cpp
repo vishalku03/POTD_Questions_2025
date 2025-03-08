c1c1023ad4d8290aea3b3842543369bc36fafc5f
Vishal Kumar <vishalk000123@gmail.com>
2025-03-08 22:21:04 +0530

    gfg

---
+++ day_64/Longest palindrome in a string.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     bool check(int i, int j, string &s)
+     {
+
+          while (i <= j)
+          {
+               if (s[i] != s[j])
+               {
+                    return false;
+               }
+               i++;
+               j--;
+          }
+          return true;
+     }
+     string longestPalindrome(string &s)
+     {
+
+          int n = s.length();
+          string ans = "";
+          string t = "";
+
+          for (int i = 0; i < n; i++)
+          {
+               for (int j = i; j < n; j++)
+               {
+                    if (check(i, j, s))
+                    {
+                         t = s.substr(i, j - i + 1);
+                         if (t.length() > ans.length())
+                         {
+                              ans = t;
+                              t = "";
+                         }
+                    }
+               }
+          }
+          return ans;
+     }
+};
+
+// gfg
