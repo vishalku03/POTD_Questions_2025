6337bcab4a0814a12de71ddb1d7a62b4c0d0209c
Vishal Kumar <vishalk000123@gmail.com>
2025-03-11 23:13:39 +0530

    lc

---
+++ day_66/Number of Substrings Containing All Three Characters.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int numberOfSubstrings(string s)
+     {
+          int abc[3] = {-1, -1, -1};
+          int count = 0, right = 0;
+          while (right < s.length())
+          {
+               abc[s[right] - 'a'] = right;
+               int minIndex = INT_MAX;
+               for (int i = 0; i < 3; i++)
+               {
+                    minIndex = min(minIndex, abc[i]);
+               }
+               count += (minIndex + 1);
+               right++;
+          }
+          return count;
+     }
+};
+
+// lc =  1358
