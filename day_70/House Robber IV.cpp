f221642dde583880938fbb9c6fc73f09cf19b9f0
Vishal Kumar <vishalk000123@gmail.com>
2025-03-15 22:56:24 +0530

    lc

---
+++ day_70/House Robber IV.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int minCapability(vector<int> &nums, int k)
+     {
+          int n = nums.size();
+
+          auto chk = [&](int c) -> bool
+          {
+               int cnt = 0;
+
+               for (int i = 0; i < n; ++i)
+               {
+                    if (nums[i] <= c)
+                    {
+                         cnt++;
+                         i++;
+                    }
+               }
+
+               return cnt >= k;
+          };
+
+          int l = 1, r = 1e9, mid;
+          while (l <= r)
+          {
+               mid = l + (r - l) / 2;
+
+               if (chk(mid))
+               {
+                    r = mid - 1;
+               }
+               else
+               {
+                    l = mid + 1;
+               }
+          }
+
+          return r + 1;
+     }
+};
+
+// lc -  2560
