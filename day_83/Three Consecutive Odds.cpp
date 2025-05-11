a3a17f638298b446873ddd3dae4ccf6d2c5d8479
Vishal Kumar <vishalk000123@gmail.com>
2025-05-11 20:45:25 +0530

    lc

---
+++ day_83/Three Consecutive Odds.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     bool threeConsecutiveOdds(vector<int> &arr)
+     {
+          int odds = 0;
+          for (auto i = 0; i < arr.size() && odds < 3; ++i)
+               odds = arr[i] % 2 ? odds + 1 : 0;
+          return odds == 3;
+     }
+};
+
+// lc -  1550
