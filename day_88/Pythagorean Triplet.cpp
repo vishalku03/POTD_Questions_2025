2ef18d483bdcaa1b27c8ddf93ac7d438a55a881f
Vishal Kumar <vishalk000123@gmail.com>
2025-05-25 19:46:18 +0530

    gfg

---
+++ day_88/Pythagorean Triplet.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     bool pythagoreanTriplet(vector<int> &arr)
+     {
+          unordered_map<int, int> mp;
+          for (int target = 0; target < arr.size(); target++)
+          {
+               int targetsum = arr[target] * arr[target];
+
+               for (int i = 0; i < arr.size(); i++)
+               {
+                    if (i == target)
+                         continue;
+                    if (mp.find(targetsum - (arr[i] * arr[i])) != mp.end())
+                    {
+                         return true;
+                    }
+                    mp[arr[i] * arr[i]]++;
+               }
+          }
+          return false;
+     }
+};
+
+// gfg
