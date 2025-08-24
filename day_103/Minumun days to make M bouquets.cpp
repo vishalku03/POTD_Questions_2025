1c8cbe15429c0fc0314f0f69521f0902b07bbdf0
Vishal Kumar <vishalk000123@gmail.com>
2025-08-24 22:07:14 +0530

    gfg

---
+++ day_103/Minumun days to make M bouquets.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int minDaysBloom(vector<int> &arr, int k, int m)
+     {
+          sort(arr.begin(), arr.end());
+          if (arr.size() < m * k)
+               return -1;
+          return arr[m * k - 1];
+     }
+};
+
+// gfg
