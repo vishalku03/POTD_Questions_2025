008cf499b625b3c72dbbe9a7b647ae83fea437cd
Vishal Kumar <vishalk000123@gmail.com>
2025-08-28 22:54:27 +0530

    gfg

---
+++ day_107/Maximize Number of 1's.cpp
@@ -0,0 +1,31 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+     public:
+     int maxOnes(vector<int> &arr, int k)
+     {
+          // code here
+          int maxi = 0;
+          int zeroCount = 0;
+          int left = 0;
+          for (int right = 0; right < arr.size(); right++)
+          {
+               if (arr[right] == 0)
+               {
+                    zeroCount++;
+               }
+               while (zeroCount > k)
+               {
+                    if (arr[left] == 0)
+                         zeroCount--;
+                    left++;
+               }
+               maxi = max(maxi, right - left + 1);
+          }
+          return maxi;
+     }
+};
+
+// gfg
