df46c846beed282bc67a2514227d54386199d976
Vishal Kumar <vishalk000123@gmail.com>
2025-09-17 20:46:55 +0530

    gfg

---
+++ day_125/Decode string.cpp
@@ -0,0 +1,50 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     string decodedString(string &s)
+     {
+          stack<int> countStack;
+          stack<string> stringStack;
+          string current = "";
+          int num = 0;
+
+          for (char c : s)
+          {
+               if (isdigit(c))
+               {
+                    num = num * 10 + (c - '0'); // build number (can be > 9)
+               }
+               else if (c == '[')
+               {
+                    countStack.push(num);
+                    stringStack.push(current);
+                    num = 0;
+                    current = "";
+               }
+               else if (c == ']')
+               {
+                    int repeat = countStack.top();
+                    countStack.pop();
+                    string prev = stringStack.top();
+                    stringStack.pop();
+
+                    string temp = "";
+                    for (int i = 0; i < repeat; i++)
+                    {
+                         temp += current;
+                    }
+                    current = prev + temp;
+               }
+               else
+               {
+                    current += c; // normal character
+               }
+          }
+          return current;
+     }
+};
+
+// gfg
