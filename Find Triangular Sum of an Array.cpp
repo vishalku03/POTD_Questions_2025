9d29b922ecfdff8d575a06f90e9a4b0f0afc2628
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2025-09-30 13:38:16 +0530

    Add files via upload

---
+++ Find Triangular Sum of an Array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+     int triangularSum(vector<int> &nums)
+     {
+          while (nums.size() != 1)
+          {
+               vector<int> ans;
+               for (int i = 1; i < nums.size(); i++)
+               {
+                    ans.push_back((nums[i] + nums[i - 1]) % 10);
+               }
+
+               nums = ans;
+          }
+
+          return nums[0];
+     }
+};
+
+// leetcode -  2221
